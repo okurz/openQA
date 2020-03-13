f96194d026e6ff9da3513628dbf5f8fe8289cb1d
Oliver Kurz <okurz@suse.de>
2020-03-13 12:21:16 +0100

    wip -- simplify and add die message for testing in WebAPI::Controller::API::V1::Job

--- lib/OpenQA/WebAPI/Controller/API/V1/Job.pm
+++ lib/OpenQA/WebAPI/Controller/API/V1/Job.pm
@@ -1,5 +1,17 @@
-# Copyright 2015-2021 SUSE LLC
-# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2015-2021 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
 
 package OpenQA::WebAPI::Controller::API::V1::Job;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
@@ -400,10 +412,12 @@ sub update_status {
     my $use_assigned_worker;
     if (!$worker && !defined $job->t_finished) {
         if (my $assigned_worker = $job->assigned_worker) {
+            die 'need to reach here FOR FLAKY: 5';
             $worker = $assigned_worker;
             $use_assigned_worker = 1;
         }
         else {
+            die 'need to reach here FOR FLAKY: 6';
             my $job_status = $job->status_info;
             my $err = "Got status update for job $job_id and worker $worker_id but there is"
               . " not even a worker assigned to this job (job is $job_status)";
