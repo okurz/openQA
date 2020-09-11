93eaf99d23b96d641c2c9fdb7057d5a5c390dd56
Oliver Kurz <okurz@suse.de>
2020-09-11 07:45:15 +0200

    Make message about clone error unique
    Previously we used the same message in two locations

--- lib/OpenQA/Schema/Result/Jobs.pm
+++ lib/OpenQA/Schema/Result/Jobs.pm
@@ -628,7 +628,7 @@ sub _create_clone {
     # not be created, somebody else was faster at cloning)
     my $orig_id       = $self->id;
     my $affected_rows = $rset->search({id => $orig_id, clone_id => undef})->update({clone_id => $new_job->id});
-    die "Job $orig_id has already been cloned as " . $self->clone_id . "\n" unless $affected_rows == 1;
+    die "Unable to clone: Job $orig_id already has clone " . $self->clone_id . "\n" unless $affected_rows == 1;
 
     # Needed to load default values from DB
     $new_job->discard_changes;
