okurz/openQA

commit 3d3dfc9a9507a389cb4d6a9ec5b085f84fc635ac

Makefile: Prevent unexpected self-test run time increases with timeouts
As https://progress.opensuse.org/issues/63985 showed it can happen that
tests increase in the run time they take without us noticing at the time
when a change is introduced. Would be good if we can prevent tests
taking significantly longer than they did before.

With this commit significant increases of test run time can be caught
when running tests locally on a global level as well as on the level of
test groups which is also effective for circle CI. Current timeout
values are a conservative limit rounded up from current run times on
circle CI which seems to provide reliable and reproducible test run
times.

The timeout value is scaled with the number of applied retries as well.

Related progress issue: https://progress.opensuse.org/issues/64216

Makefile
@@ -18,6 +18,9 @@ endif
 PROVE_LIB_ARGS ?= -l
 DOCKER_IMG ?= openqa:latest
 TEST_PG_PATH ?= /dev/shm/tpg
+# TIMEOUT_M: Timeout for one retry of tests in minutes
+TIMEOUT_M ?= 60
+TIMEOUT_RETRIES ?= $$((${TIMEOUT_M} * (${RETRY} + 1) ))m
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))
 docker_env_file := "$(current_dir)/docker.env"
@@ -128,32 +131,32 @@ test-checkstyle: test-checkstyle-standalone test-tidy-compile
 
 .PHONY: test-t
 test-t:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 .PHONY: test-ui
 test-ui:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 .PHONY: test-api
 test-api:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=15 PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 # put unstable tests in unstable_tests.txt and uncomment in circle CI to handle unstables with retries
 .PHONY: test-unstable
 test-unstable:
-	for f in $$(cat .circleci/unstable_tests.txt); do $(MAKE) test-with-database PROVE_ARGS="$$HARNESS $f" RETRY=3 || break; done
+	for f in $$(cat .circleci/unstable_tests.txt); do $(MAKE) test-with-database TIMEOUT_M=5 PROVE_ARGS="$$HARNESS $f" RETRY=3 || break; done
 
 .PHONY: test-fullstack
 test-fullstack:
-	$(MAKE) test-with-database FULLSTACK=1 PROVE_ARGS="$$HARNESS t/full-stack.t" RETRY=3
+	$(MAKE) test-with-database FULLSTACK=1 TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/full-stack.t" RETRY=3
 
 .PHONY: test-scheduler
 test-scheduler:
-	$(MAKE) test-with-database SCHEDULER_FULLSTACK=1 SCALABILITY_TEST=1 PROVE_ARGS="$$HARNESS t/05-scheduler-full.t t/43-scheduling-and-worker-scalability.t" RETRY=3
+	$(MAKE) test-with-database SCHEDULER_FULLSTACK=1 SCALABILITY_TEST=1 TIMEOUT_M=5 PROVE_ARGS="$$HARNESS t/05-scheduler-full.t t/43-scheduling-and-worker-scalability.t" RETRY=3
 
 .PHONY: test-developer
 test-developer:
-	$(MAKE) test-with-database DEVELOPER_FULLSTACK=1 PROVE_ARGS="$$HARNESS t/33-developer_mode.t" RETRY=3
+	$(MAKE) test-with-database DEVELOPER_FULLSTACK=1 TIMEOUT_M=10 PROVE_ARGS="$$HARNESS t/33-developer_mode.t" RETRY=3
 
 .PHONY: test-with-database
 test-with-database:
@@ -164,7 +167,7 @@ test-with-database:
 .PHONY: test-unit-and-integration
 test-unit-and-integration:
 	export GLOBIGNORE="$(GLOBIGNORE)";\
-	script/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	timeout -v ${TIMEOUT_RETRIES} script/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
 
 # prepares running the tests within Docker (eg. pulls os-autoinst) and then runs the tests considering
 # the test matrix environment variables
@@ -237,7 +240,7 @@ test-critic:
 
 .PHONY: test-tidy-compile
 test-tidy-compile:
-	$(MAKE) test-unit-and-integration PROVE_ARGS="$$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$(unstables)"
+	$(MAKE) test-unit-and-integration TIMEOUT_M=15 PROVE_ARGS="$$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$(unstables)"
 
 .PHONY: test-shellcheck
 test-shellcheck:

commit a230c6d9389d738b5d30422c65867b62da7025f6

Add test for Makefile+retry behaviour
WIP -- check t/50-make-retry , still does not work, low level command aborted with ctrl-c does not show result and make still is timed out put IPC::Run timeout, maybe due to a tclsh zombie process stuck -- Fix passing signals in make jobs to tests

This fixes a problem introduced in 3d3dfc9a9 that prevents being able to
abort tests with Ctrl-C when started with make.

andriinikitin suggests: "[13/05/2020 12:38:53] <andriinikitin> Maybe you don't even need perl based solution here. I did investigate this in bash and what seems to work: 1. start new tests in new process group using setsid bash myscript.sh 2. In myscript.sh on exit do kill whole process group kill -- -$$, e.g. trap 'test -z "$intrap" && export intrap=1 && kill -- -$$ 2>/dev/null || :' SIGINT SIGTERM EXIT . the approach seems to be correct, maybe it can work like doing setsid prove ...; set pg=$? and then kill whole processgroup kill -- -$pg"

wip -- maybe I just need to catch INT in 50-make-retry as normally ctrl-c would send INT to all processes in a process group

wip

Makefile
@@ -162,14 +162,28 @@ test-developer:
 # only works with one and Fedora's build system only works with the other
 .PHONY: test-with-database
 test-with-database:
-	test -d $(TEST_PG_PATH) && (pg_ctl -D $(TEST_PG_PATH) -s status >&/dev/null || pg_ctl -D $(TEST_PG_PATH) -s start) || ./t/test_postgresql $(TEST_PG_PATH)
+	@test -d $(TEST_PG_PATH) && (pg_ctl -D $(TEST_PG_PATH) -s status >&/dev/null || pg_ctl -D $(TEST_PG_PATH) -s start) || ./t/test_postgresql $(TEST_PG_PATH)
 	PERL5OPT="$(PERL5OPT) -It/lib -I$(PWD)/t/lib -MOpenQA::Test::PatchDeparse" $(MAKE) test-unit-and-integration TEST_PG="DBI:Pg:dbname=openqa_test;host=$(TEST_PG_PATH)"
-	-[ $(KEEP_DB) = 1 ] || pg_ctl -D $(TEST_PG_PATH) stop
+	@-[ $(KEEP_DB) = 1 ] || pg_ctl -D $(TEST_PG_PATH) stop
 
 .PHONY: test-unit-and-integration
+# 'timeout --foreground' to still pass signals to subprocesses, e.g. ctrl-c
 test-unit-and-integration:
 	export GLOBIGNORE="$(GLOBIGNORE)";\
-	RETRY=${RETRY} timeout -s SIGINT -k 5 -v ${TIMEOUT_RETRIES} tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#timeout -s SIGINT -k 5 -v ${TIMEOUT_RETRIES} tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#timeout -v ${TIMEOUT_RETRIES} tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#tools/retry timeout --foreground -v ${TIMEOUT_RETRIES} prove --trap ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	# TODO setting timeout internal in tools/retry seems promising but this
+	# unfortunately leaves child processes behind, maybe prove with --trap?
+	#TIMEOUT=${TIMEOUT_RETRIES} tools/retry prove --trap ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	RETRY=${RETRY} prove --trap ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#TIMEOUT=${TIMEOUT_RETRIES} bash -ex tools/retry prove --trap ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#TIMEOUT=${TIMEOUT_RETRIES} setsid bash -ex tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#timeout --foreground -v ${TIMEOUT_RETRIES} setsid tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#unbuffer timeout --foreground -v ${TIMEOUT_RETRIES} tools/retry /usr/bin/prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#timeout -v ${TIMEOUT_RETRIES} tools/retry /usr/bin/prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	#timeout --foreground -v 3 tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
 
 # prepares running the tests within Docker (eg. pulls os-autoinst) and then runs the tests considering
 # the test matrix environment variables

tools/retry
@@ -1,14 +1,48 @@
 #!/bin/sh -e
 # shellcheck disable=SC2048,SC2086
 
+set -exo pipefail
+
+#if  [ $$ = $(ps -o pgid -hp $$) ]; then
+#   echo already a process group leader;
+#else
+#   set -m
+#   $0 "$@" #optionally with &
+#   set +m
+#fi
+
+env | grep '\(RETRY\|STABILITY_TEST\|TIMEOUT\)' ||:
+
 RETRY="${RETRY:-3}"
 STABILITY_TEST="${STABILITY_TEST:-0}"
+#TIMEOUT="${TIMEOUT
+
+trap
+
+# Ensure subprocesses are terminated if only this program receives signals
+#trap "exit" INT
+#trap "exit" INT
+#trap "echo CAUGHT signal in $$; ps Tf -o '%p %P %r %y %a' | tail -n 10; kill -- -$$; exit" EXIT
+#trap "echo CAUGHT signal in $$; ps Tf -o '%p %P %r %y %a' | tail -n 10; exit" TERM INT
+#trap 'echo caught interrupt and exiting;exit' INT
+#trap "echo CAUGHT signal in $$; ps Tf -o '%p %P %r %y %x %a'; kill $(jobs -p)&>/dev/null; kill -- -$$; exit" EXIT
+#trap 'kill $(jobs -p) 2>/dev/null' EXIT
+#trap 'kill $(jobs -p) 2>/dev/null' INT
+#trap "trap - SIGTERM && kill -- -$$" INT TERM EXIT
+#trap "exit \$exit_code" INT TERM
+#trap "exit_code=\$?; kill 0" EXIT
+#trap 'echo TRAP: kill -- -$BASHPID' INT TERM
+#trap 'echo "IN TRAP"; kill 0' INT
 
 run_once() {
-    $*
+    if [ -n "$TIMEOUT" ]; then
+        prefix="timeout --foreground -v $TIMEOUT"
+    fi
+    $prefix $*
 }
 
 if [ "$RETRY" = "0" ]; then
+    #echo run_once $*
     run_once $*
 else
     n=1

t/50-make-retry.t
@@ -1,0 +1,118 @@
+#!/usr/bin/env perl
+# Copyright (C) 2020 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+use Test::Most;
+use Test::Warnings;
+use FileHandle;
+use IPC::Run qw(run start timeout pump signal);
+
+# Ensure there is output from prove calls showing the status on test
+# executions as soon as test modules start
+STDOUT->autoflush(1);
+STDERR->autoflush(1);
+
+my ($out, $err);
+
+sub start_once {
+   my ($cmd, %args) = @_;
+   ($out, $err) = ('', '');
+   $args{timeout} //= 4;
+   start $cmd, \undef, \$out, \$err, timeout($args{timeout})
+}
+
+ok run(qw(tools/retry true)), 'tools/retry can be called with success';
+#ok run(qw(make help)), 'make can be called with success';
+my @simple_test = qw(make test KEEP_DB=1 TESTS=t/config.t);
+my $h = start_once \@simple_test;
+ok $h->finish, 'simple test with make successful';
+like $out, qr/All tests successful/, 'internal test successful';
+# if I remove "unbuffer" then this test is hard aborted as soon as
+# long_test_1s is aborted on timeout. If I keep it then unbuffer is left
+# around as a zombie process when I signal long test later.
+my @long_test = qw(unbuffer make test KEEP_DB=1 TESTS=t/ui/01-list.t);
+my @long_test_1s = (@long_test, qw(TIMEOUT_RETRIES=1s));
+$h = start_once \@long_test_1s;
+ok $h, 'long test started';
+ok ! $h->finish, 'test could not succeed within given time' or diag "out: $out\nerr: $err";
+note "out: $out\nerr: $err";
+is $h->result, 2, 'test exceeding timeout is aborted with timeout failure';
+# old variant using GNU "timeout" command which has problems to be aborted
+# with Ctrl-C
+like $out, qr/timeout: sending signal TERM/, 'timeout terminated test';
+#like $out, qr/Timed out/, 'timeout terminated test';
+note "ps: " . qx{ps Tf | tail -n 10};
+#$h = start_once \@long_test, timeout => 10;
+# TODO check first if the low-level tools/retry call can be aborted with
+# ctrl-c
+my @long_test_low_level = qw(setsid unbuffer tools/retry prove -l t/ui/01-list.t);
+$h = start_once \@long_test_low_level;
+#$h = start_once \@long_test;
+# TODO check for "unbuffer" again at the right location
+ok $h, 'started long test to abort it with signal later';
+pump $h until $out =~ qr{t/ui/01-list.t \.\.};
+note "ps: " . qx{ps Tf -o '%p %P %r %y %x %a'};
+note "out: $out\nerr: $err";
+pass 'long test has started t/ui/01-list.t, simulating ctrl-C on test run';
+$SIG{INT} = sub { diag "50-make-retry.t: Found INT" };
+$h->signal('INT');
+#ok signal($h, 'INT'), 'simulating ctrl-C on test run';
+#ok signal($h, 'QUIT'), 'QUIT';
+#ok signal($h, 'HUP'), 'HUP';
+#ok signal($h, 'TERM'), 'TERM';
+#ok signal($h, 'KILL'), 'KILL';
+note "ps: " . qx{ps Tf -o '%p %P %r %y %x %a'};
+ok ! $h->finish, 'test was aborted due to simulated ctrl-c';
+#is $h->result, 2, 'test aborted with corresponding exit code';
+is $h->full_result, 2, 'test aborted with corresponding exit code';
+#is_deeply @{$h->full_results}, [2], 'test aborted with corresponding exit code';
+#note "result/results/full_result/full_results: " . join('/', $h->result, $h->results, $h->full_result, $h->full_results);
+$h = start_once \@long_test;
+ok $h, 'started long test with make to abort it with signal later';
+pump $h until $out =~ qr{t/ui/01-list.t \.\.};
+note "ps after start: " . qx{ps Tf -o '%p %P %r %y %x %a'};
+note "pgrep: after start " . qx{pgrep -f -a perl};
+note "out: $out\nerr: $err";
+pass 'long test with make has started t/ui/01-list.t, simulating ctrl-C on test run';
+$h->signal('INT');
+# just sending INT to the parent process is not the same as on terminal, we
+# need to simulate an interactive bash session behaviour by sending INT to all
+# processes in the process group
+#kill '-INT', $h->{KIDS}->[0]->{PID};
+# TODO there seem to be no processes left in tree but pgrep still reports left
+# overs, I need to find the relation between left-over processes the tree.
+# maybe iterate over children as well and crosscheck behaviour in interactive
+# bash
+kill '-INT', $$;
+note "ps after int: " . qx{ps Tf -o '%p %P %r %y %x %a'};
+note "pgrep: " . qx{pgrep -f -a perl};
+note 'before finish, pumping';
+pump_nb $h;
+note 'before finish, cleaning up potential defunct children that would block finish';
+$h->reap_nb;
+note "ps after reap: " . qx{ps Tf -o '%p %P %r %y %x %a'};
+note "pgrep after reap: " . qx{pgrep -f -a perl};
+$h->kill_kill(grace => 1);
+note "result/results/full_result/full_results: " . join('/', $h->result, $h->results, $h->full_result, $h->full_results);
+#ok ! $h->finish, 'test with make was aborted due to simulated ctrl-c';
+#is $h->result, 2, 'test with make aborted with corresponding exit code';
+is $h->full_result, 2, 'test with make aborted with corresponding exit code';
+
+done_testing;
+
+END {
+note "ps: " . qx{ps Tf -o '%p %P %r %y %x %a' | tail -n 10};
+    
+    defined $h and kill_kill $h, grace => 1 }

t/config.t
@@ -191,8 +191,8 @@ subtest 'trim whitespace characters from both ends of openqa.ini value' => sub {
     ';
     $t_dir->child('openqa.ini')->spurt($data);
     OpenQA::Setup::read_config($app);
-    print $app->config->{global}->{appname};
-    print $app->config->{global}->{hide_asset_types};
+    note $app->config->{global}->{appname};
+    note $app->config->{global}->{hide_asset_types};
     ok($app->config->{global}->{appname} eq 'openQA',            'appname');
     ok($app->config->{global}->{hide_asset_types} eq 'repo iso', 'hide_asset_types');
     is_deeply(

t/ui/01-list.t
@@ -15,6 +15,12 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
 use Test::Most;
+pass 'begin';
+sleep 3600;
+pass 'good';
+done_testing;
+
+__END__
 
 use FindBin;
 use lib "$FindBin::Bin/../lib", "$FindBin::Bin/../../external/os-autoinst-common/lib";