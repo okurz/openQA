okurz/openQA

commit 3d3dfc9a9507a389cb4d6a9ec5b085f84fc635ac

Makefile: Prevent unexpected self-test run time increases with timeouts
As https://progress.opensuse.org/issues/63985 showed it can happen that
tests increase in the run time they take without us noticing at the time
when a change is introduced. Would be good if we can prevent tests
taking significantly longer than they did before.

With this commit significant increases of test run time can be caught
when running tests locally on a global level as well as on the level of
test groups which is also effective for circle CI. Current timeout
values are a conservative limit rounded up from current run times on
circle CI which seems to provide reliable and reproducible test run
times.

The timeout value is scaled with the number of applied retries as well.

Related progress issue: https://progress.opensuse.org/issues/64216

Makefile
@@ -18,6 +18,9 @@ endif
 PROVE_LIB_ARGS ?= -l
 DOCKER_IMG ?= openqa:latest
 TEST_PG_PATH ?= /dev/shm/tpg
+# TIMEOUT_M: Timeout for one retry of tests in minutes
+TIMEOUT_M ?= 60
+TIMEOUT_RETRIES ?= $$((${TIMEOUT_M} * (${RETRY} + 1) ))m
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))
 docker_env_file := "$(current_dir)/docker.env"
@@ -128,32 +131,32 @@ test-checkstyle: test-checkstyle-standalone test-tidy-compile
 
 .PHONY: test-t
 test-t:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 .PHONY: test-ui
 test-ui:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 .PHONY: test-api
 test-api:
-	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=15 PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
 
 # put unstable tests in unstable_tests.txt and uncomment in circle CI to handle unstables with retries
 .PHONY: test-unstable
 test-unstable:
-	for f in $$(cat .circleci/unstable_tests.txt); do $(MAKE) test-with-database PROVE_ARGS="$$HARNESS $f" RETRY=3 || break; done
+	for f in $$(cat .circleci/unstable_tests.txt); do $(MAKE) test-with-database TIMEOUT_M=5 PROVE_ARGS="$$HARNESS $f" RETRY=3 || break; done
 
 .PHONY: test-fullstack
 test-fullstack:
-	$(MAKE) test-with-database FULLSTACK=1 PROVE_ARGS="$$HARNESS t/full-stack.t" RETRY=3
+	$(MAKE) test-with-database FULLSTACK=1 TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/full-stack.t" RETRY=3
 
 .PHONY: test-scheduler
 test-scheduler:
-	$(MAKE) test-with-database SCHEDULER_FULLSTACK=1 SCALABILITY_TEST=1 PROVE_ARGS="$$HARNESS t/05-scheduler-full.t t/43-scheduling-and-worker-scalability.t" RETRY=3
+	$(MAKE) test-with-database SCHEDULER_FULLSTACK=1 SCALABILITY_TEST=1 TIMEOUT_M=5 PROVE_ARGS="$$HARNESS t/05-scheduler-full.t t/43-scheduling-and-worker-scalability.t" RETRY=3
 
 .PHONY: test-developer
 test-developer:
-	$(MAKE) test-with-database DEVELOPER_FULLSTACK=1 PROVE_ARGS="$$HARNESS t/33-developer_mode.t" RETRY=3
+	$(MAKE) test-with-database DEVELOPER_FULLSTACK=1 TIMEOUT_M=10 PROVE_ARGS="$$HARNESS t/33-developer_mode.t" RETRY=3
 
 .PHONY: test-with-database
 test-with-database:
@@ -164,7 +167,7 @@ test-with-database:
 .PHONY: test-unit-and-integration
 test-unit-and-integration:
 	export GLOBIGNORE="$(GLOBIGNORE)";\
-	script/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	timeout -v ${TIMEOUT_RETRIES} script/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
 
 # prepares running the tests within Docker (eg. pulls os-autoinst) and then runs the tests considering
 # the test matrix environment variables
@@ -237,7 +240,7 @@ test-critic:
 
 .PHONY: test-tidy-compile
 test-tidy-compile:
-	$(MAKE) test-unit-and-integration PROVE_ARGS="$$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$(unstables)"
+	$(MAKE) test-unit-and-integration TIMEOUT_M=15 PROVE_ARGS="$$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$(unstables)"
 
 .PHONY: test-shellcheck
 test-shellcheck:

commit f1b360a9e98a083f3a429eb6f6d3eb815825324c

Fix passing signals in make jobs to tests
This fixes a problem introduced in 3d3dfc9a9 that prevents being able to
abort tests with Ctrl-C when started with make.

Makefile
@@ -167,9 +167,10 @@ test-with-database:
 	-[ $(KEEP_DB) = 1 ] || pg_ctl -D $(TEST_PG_PATH) stop
 
 .PHONY: test-unit-and-integration
+# 'timeout --foreground' to still pass signals to subprocesses, e.g. ctrl-c
 test-unit-and-integration:
 	export GLOBIGNORE="$(GLOBIGNORE)";\
-	RETRY=${RETRY} timeout -s SIGINT -k 5 -v ${TIMEOUT_RETRIES} tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
+	RETRY=${RETRY} timeout --foreground -s SIGINT -k 5 -v ${TIMEOUT_RETRIES} tools/retry prove ${PROVE_LIB_ARGS} ${PROVE_ARGS}
 
 # prepares running the tests within Docker (eg. pulls os-autoinst) and then runs the tests considering
 # the test matrix environment variables

tools/retry
@@ -4,6 +4,9 @@
 RETRY="${RETRY:-3}"
 STABILITY_TEST="${STABILITY_TEST:-0}"
 
+# Ensure subprocesses are terminated if only this program receives signals
+trap 'kill 0' EXIT
+
 run_once() {
     $*
 }